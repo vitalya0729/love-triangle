76cab13ce2dd4c110dd4ea91ee02fb9aeae0a695
vitalya0729 <36700292+vitalya0729@users.noreply.github.com>
2018-02-22 10:39:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var num = 0;
+  for (var i = 0; i < preferences.length; i++){
+   if (preferences[preferences[preferences[i]-1]-1]-1]-1 == 1){
+     num++;
+   }
+  }
+  return num = num/3;
 };
